f6d8c87830070e2cbb11e5acb6485e76229970f4
malierbay <malierbay9@gmail.com>
2022-07-03 01:09:55 +0300

    Mehmet Ali Erbay homework-2

--- homework2/DoTest.java
+++ homework2/DoTest.java
@@ -1,16 +1,22 @@
 package homework2;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DoTest {
 
-    @Test
+    private TestExistenceOfElement isElementExist;
+
+    @BeforeEach //BeforeEach anotasyonu altındaki metot her bir test case den önce çalışacaktır.
+    void setUp() {
+        this.isElementExist = new TestExistenceOfElement();  //Nesne üretiyoruz.
+    }
+
+    @Test // Test anotasyonu altındaki metodun test case olarak çalışacağını belirtir.
     public void test(){
 
-        TestExistenceOfElement isElementExist = new TestExistenceOfElement();// Sınıfımızdan nesne oluşturuyoruz.
-
-        Assertions.assertEquals(true,isElementExist.isExist());
+        Assertions.assertEquals(true, isElementExist.isExist());
         //junit kütüphanesine ait Assertions koşulları sayesinde testimizin sonucunu sorgulayabiliriz.
         //assertEquals metodunda beklenen değer olarak, aradığımız elementin sayfada var olduğunu varsayarak 'true' giriyoruz.
         //Bu metot, nesnemizin isExist() metodundan gelen sonuçla beklediğimiz değeri kıyaslayıp testi sonuçlandıracak.
